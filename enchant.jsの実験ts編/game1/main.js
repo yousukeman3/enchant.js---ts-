da4adb589adfcd5c6078166f9ae33734c23b6119
yousukeman3 <yousukeman3@gmail.com>
2017-09-07 22:42:19 +0900

    add enemy＆コードを成形

--- enchant.jsの実験ts編/game1/main.js
+++ enchant.jsの実験ts編/game1/main.js
@@ -1,27 +1,35 @@
+"use strict";
 ///<reference path="../enchant.js.d.ts"/>
 // code.9leap.net default template
 // based on enchant.js v0.7.1
+let myRandom = function (max, min) {
+    let _ = Math.round(Math.random() * (max + 1 - min)) + min;
+    //console.log(_);
+    return _;
+};
 enchant();
 window.onload = function () {
-    var game = new enchant.Core(320, 320);
+    let game = new enchant.Core(320, 320);
     game.fps = 15;
     game.preload("http://coderun.9leap.net/codes/132391/bg.png", "http://coderun.9leap.net/codes/132391/shooting.png");
-    var scene1 = new enchant.Scene();
-    var backgroundImage = new enchant.Sprite(320, 320);
     game.onload = function () {
+        let scene1 = new enchant.Scene();
+        let backgroundImage = new enchant.Sprite(320, 320);
         backgroundImage.image = game.assets["http://coderun.9leap.net/codes/132391/bg.png"];
         scene1.addChild(backgroundImage);
-        var ownMachine = new enchant.Sprite(32, 32);
+        let ownMachine = new enchant.Sprite(32, 32);
         ownMachine.image = game.assets["http://coderun.9leap.net/codes/132391/shooting.png"];
-        scene1.addChild(ownMachine);
-        var bullets = [];
         ownMachine.y = 320 - 32;
-        game.keybind('D'.charCodeAt(0), 'Dash');
-        game.keybind('S'.charCodeAt(0), 'Shoot');
-        var ownMachineFunc;
-        var waitBullet = 5;
-        var bulletCount = 0;
-        scene1.addEventListener('enterframe', ownMachineFunc = function () {
+        scene1.addChild(ownMachine);
+        let bullets = [];
+        let enemys = [];
+        let waitBullet = 5;
+        let bulletCount = 0;
+        let waitenemy = 0;
+        let enemyCount = 0;
+        let enemyrate = 90;
+        let enemySpeed = 5;
+        let ownMachineFunc = function () {
             if (game.input.right) {
                 ownMachine.x += 4;
                 if (game.input.Dash) {
@@ -40,19 +48,21 @@ window.onload = function () {
                     ownMachine.x = 0;
                 }
             }
-            console.log(bullets.length);
+        };
+        let bulletFunc = function () {
+            //console.log(bullets.length);
             bullets.forEach(function (bullet, index, bulletAllay) {
                 if (!(bullet == null || bullet == undefined)) {
                     bullet.y -= 10;
-                    if (bullet.y < 0) {
+                    if (bullet.y < -32) {
                         scene1.removeChild(bullet);
                         bullets.shift();
-                        console.log(bullets.length);
+                        //console.log(bullets.length);
                     }
                 }
             });
-            if (game.input.Shoot && bulletCount > 5) {
-                var index = bullets.push(new enchant.Sprite(32, 32)) - 1;
+            if (game.input.Shoot && bulletCount > waitBullet) {
+                let index = bullets.push(new enchant.Sprite(32, 32)) - 1;
                 bullets[index].image = game.assets["http://coderun.9leap.net/codes/132391/shooting.png"];
                 bullets[index].frame = 2;
                 bullets[index].x = ownMachine.x;
@@ -61,11 +71,37 @@ window.onload = function () {
                 bulletCount = 0;
             }
             bulletCount++;
-        });
+        };
+        let enemyFunc = function () {
+            enemys.forEach(function (enemy, index, enemyAllay) {
+                if (!(enemy == null || enemy == undefined)) {
+                    enemy.y += enemySpeed;
+                    if (enemy.y > 320) {
+                        scene1.removeChild(enemy);
+                        enemys.shift();
+                    }
+                }
+            });
+            if (myRandom(0, enemyrate) == 1) {
+                let index = enemys.push(new enchant.Sprite(32, 32)) - 1;
+                enemys[index].image = game.assets["http://coderun.9leap.net/codes/132391/shooting.png"];
+                enemys[index].frame = 1;
+                enemys[index].x = myRandom(0, 320 - 32);
+                enemys[index].y = -32;
+                scene1.addChild(enemys[index]);
+            }
+        };
+        let perDecision = function () {
+            bullets.forEach(function () {
+            });
+        };
+        game.keybind('D'.charCodeAt(0), 'Dash');
+        game.keybind('S'.charCodeAt(0), 'Shoot');
+        scene1.addEventListener("enterframe", ownMachineFunc);
+        scene1.addEventListener("enterframe", bulletFunc);
+        scene1.addEventListener("enterframe", enemyFunc);
         game.pushScene(scene1);
     };
     game.start();
 };
-//seで困ったらこれをつかえ
-//http://wikiwiki.jp/piporpg/?%A5%B5%A5%A6%A5%F3%A5%C9%C1%C7%BA%E0%20%C7%DB%C9%DB%A5%B5%A5%A4%A5%C8
 //# sourceMappingURL=main.js.map
--- enchant.jsの実験ts編/game1/main.ts
+++ enchant.jsの実験ts編/game1/main.ts
@@ -1,36 +1,54 @@
-﻿///<reference path="../enchant.js.d.ts"/>
+﻿"use strict"
+///<reference path="../enchant.js.d.ts"/>
 // code.9leap.net default template
 // based on enchant.js v0.7.1
+
+let myRandom = function (max: number, min: number) {
+    let _ = Math.round(Math.random() * (max + 1 - min)) + min;
+    //console.log(_);
+    return _;
+}
 
 enchant();
 
 window.onload = function () {
-    var game = new enchant.Core(320, 320);
+    let game = new enchant.Core(320, 320);
     game.fps = 15;
     game.preload("http://coderun.9leap.net/codes/132391/bg.png","http://coderun.9leap.net/codes/132391/shooting.png");
-    var scene1 = new enchant.Scene();
-    var backgroundImage = new enchant.Sprite(320, 320);
     game.onload = function () {
+
+        let scene1 = new enchant.Scene();
+        let backgroundImage = new enchant.Sprite(320, 320);
         backgroundImage.image = game.assets["http://coderun.9leap.net/codes/132391/bg.png"];
         scene1.addChild(backgroundImage);
-        var ownMachine = new enchant.Sprite(32, 32);
-        ownMachine.image = game.assets["http://coderun.9leap.net/codes/132391/shooting.png"];
-        scene1.addChild(ownMachine);
-        var bullets: enchant.Sprite[] = [];
-        ownMachine.y = 320 - 32;
-        game.keybind('D'.charCodeAt(0), 'Dash');
-        game.keybind('S'.charCodeAt(0), 'Shoot');
-        var ownMachineFunc: () => void;
-        var waitBullet = 5;
-        var bulletCount = 0;
 
-        scene1.addEventListener('enterframe',ownMachineFunc = function (){
+
+        let ownMachine = new enchant.Sprite(32, 32);
+        ownMachine.image = game.assets["http://coderun.9leap.net/codes/132391/shooting.png"];
+        ownMachine.y = 320 - 32;
+        scene1.addChild(ownMachine);
+
+
+        let bullets: enchant.Sprite[] = [];
+        let enemys: enchant.Sprite[] = [];
+
+
+        let waitBullet = 5;
+        let bulletCount = 0;
+
+        let waitenemy = 0;
+        let enemyCount = 0;
+        let enemyrate = 90;
+        let enemySpeed = 5;
+
+
+        let ownMachineFunc = function () {
             if (game.input.right) {
                 ownMachine.x += 4;
                 if (game.input.Dash) {
                     ownMachine.x += 4;
                 }
-                if (ownMachine.x > 320 - 32){
+                if (ownMachine.x > 320 - 32) {
                     ownMachine.x = 320 - 32;
                 }
             }
@@ -39,33 +57,71 @@ window.onload = function () {
                 if (game.input.Dash) {
                     ownMachine.x -= 4;
                 }
-                if (ownMachine.x < 0){
+                if (ownMachine.x < 0) {
                     ownMachine.x = 0;
                 }
-            } 
-            console.log(bullets.length);
+            }
+        };
+
+        
+        let bulletFunc = function () {
+            //console.log(bullets.length);
             bullets.forEach(function (bullet, index, bulletAllay) {
                 if (!(bullet == null || bullet == undefined)) {
                     bullet.y -= 10;
-                    if (bullet.y < 0) {
+                    
+                    if (bullet.y < -32) {
                         scene1.removeChild(bullet);
                         bullets.shift();
-                        console.log(bullets.length);
+                        //console.log(bullets.length);
                     }
                 }
             });
-            if (game.input.Shoot && bulletCount > 5) {
-                var index = bullets.push(new enchant.Sprite(32, 32)) - 1;
+            if (game.input.Shoot && bulletCount > waitBullet) {
+                let index = bullets.push(new enchant.Sprite(32, 32)) - 1;
                 bullets[index].image = game.assets["http://coderun.9leap.net/codes/132391/shooting.png"];
                 bullets[index].frame = 2;
                 bullets[index].x = ownMachine.x;
                 bullets[index].y = ownMachine.y - 32;
                 scene1.addChild(bullets[index]);
                 bulletCount = 0;
-
             }
             bulletCount++;
-        });
+        };
+
+        let enemyFunc = function () {
+            enemys.forEach(function (enemy, index, enemyAllay) {
+                if (!(enemy == null || enemy == undefined)) {
+                    enemy.y += enemySpeed;
+
+                    if (enemy.y > 320) {
+                        scene1.removeChild(enemy);
+                        enemys.shift();
+                    }
+                }
+            });
+            if (myRandom(0, enemyrate) == 1) {
+                let index = enemys.push(new enchant.Sprite(32, 32)) - 1;
+                enemys[index].image = game.assets["http://coderun.9leap.net/codes/132391/shooting.png"];
+                enemys[index].frame = 1;
+                enemys[index].x = myRandom(0, 320 - 32);
+                enemys[index].y = -32;
+                scene1.addChild(enemys[index]);
+            }
+        }
+
+        let perDecision = function () {
+            bullets.forEach(function () {
+
+            });
+        }
+
+        game.keybind('D'.charCodeAt(0), 'Dash');
+        game.keybind('S'.charCodeAt(0), 'Shoot');
+
+        scene1.addEventListener("enterframe", ownMachineFunc);
+        scene1.addEventListener("enterframe", bulletFunc);
+        scene1.addEventListener("enterframe", enemyFunc);
         game.pushScene(scene1);
     };
     game.start();
