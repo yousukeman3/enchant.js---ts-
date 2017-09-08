805db0d21695eb09f8d30e986d122c083a55aace
yousukeman3 <yousukeman3@gmail.com>
2017-09-09 00:09:21 +0900

    完成
    よーくがんばったよ

--- enchant.jsの実験ts編/game1/main.js
+++ enchant.jsの実験ts編/game1/main.js
@@ -11,16 +11,25 @@ enchant();
 window.onload = function () {
     let game = new enchant.Core(320, 320);
     game.fps = 15;
-    game.preload("http://coderun.9leap.net/codes/132391/bg.png", "http://coderun.9leap.net/codes/132391/shooting.png");
+    game.preload("bg.png", "shooting.png");
     game.onload = function () {
         let scene1 = new enchant.Scene();
         let backgroundImage = new enchant.Sprite(320, 320);
-        backgroundImage.image = game.assets["http://coderun.9leap.net/codes/132391/bg.png"];
+        backgroundImage.image = game.assets["bg.png"];
         scene1.addChild(backgroundImage);
         let ownMachine = new enchant.Sprite(32, 32);
-        ownMachine.image = game.assets["http://coderun.9leap.net/codes/132391/shooting.png"];
+        ownMachine.image = game.assets["shooting.png"];
         ownMachine.y = 320 - 32;
         scene1.addChild(ownMachine);
+        let score = 0;
+        let scoreLabel = new enchant.Label();
+        scoreLabel.text = "score:" + score;
+        scoreLabel.color = "white";
+        scene1.addChild(scoreLabel);
+        let gameover = new enchant.Sprite(190, 97);
+        gameover.image = game.assets["end.png"];
+        gameover.x = (320 - 190) / 2;
+        gameover.y = (320 - 97) / 2;
         let bullets = [];
         let enemys = [];
         let waitBullet = 5;
@@ -29,6 +38,18 @@ window.onload = function () {
         let enemyCount = 0;
         let enemyrate = 45;
         let enemySpeed = 5;
+        let gameoverFunc = function () {
+            if (game.input.Retry) {
+                scene1.removeChild(gameover);
+                score = 0;
+                scoreLabel.text = "score:" + score;
+                scene1.removeEventListener("enterframe", gameoverFunc);
+                scene1.addEventListener("enterframe", ownMachineFunc);
+                scene1.addEventListener("enterframe", bulletFunc);
+                scene1.addEventListener("enterframe", enemyFunc);
+                scene1.addEventListener("enterframe", perDecision);
+            }
+        };
         let ownMachineFunc = function () {
             if (game.input.right) {
                 ownMachine.x += 4;
@@ -63,7 +84,7 @@ window.onload = function () {
             });
             if (game.input.Shoot && bulletCount > waitBullet) {
                 let index = bullets.push(new enchant.Sprite(32, 32)) - 1;
-                bullets[index].image = game.assets["http://coderun.9leap.net/codes/132391/shooting.png"];
+                bullets[index].image = game.assets["shooting.png"];
                 bullets[index].frame = 2;
                 bullets[index].x = ownMachine.x;
                 bullets[index].y = ownMachine.y - 32;
@@ -84,7 +105,7 @@ window.onload = function () {
             });
             if (myRandom(0, enemyrate) == 1) {
                 let index = enemys.push(new enchant.Sprite(32, 32)) - 1;
-                enemys[index].image = game.assets["http://coderun.9leap.net/codes/132391/shooting.png"];
+                enemys[index].image = game.assets["shooting.png"];
                 enemys[index].frame = 1;
                 enemys[index].x = myRandom(0, 320 - 32);
                 enemys[index].y = -32;
@@ -92,14 +113,44 @@ window.onload = function () {
             }
         };
         let perDecision = function () {
-            bullets.forEach(function () {
+            bullets.forEach(function (bullet, indexb, bulletAllay) {
+                enemys.forEach(function (enemy, index, enemyAllay) {
+                    //console.log("〇");
+                    if (bullet.within(enemy, 16)) {
+                        scene1.removeChild(enemy);
+                        enemys.splice(index, 1);
+                        score++;
+                        scoreLabel.text = "score:" + score;
+                    }
+                });
+            });
+            enemys.forEach(function (enemy, index, enemyAllay) {
+                if (enemy.within(ownMachine, 16) || enemy.y > 320 - 32) {
+                    //gameoverだお
+                    scene1.removeEventListener("enterframe", ownMachineFunc);
+                    scene1.removeEventListener("enterframe", bulletFunc);
+                    scene1.removeEventListener("enterframe", enemyFunc);
+                    scene1.removeEventListener("enterframe", perDecision);
+                    while (enemys.length > 0) {
+                        scene1.removeChild(enemys[0]);
+                        enemys.shift();
+                    }
+                    while (bullets.length > 0) {
+                        scene1.removeChild(bullets[0]);
+                        enemys.shift();
+                    }
+                    scene1.addChild(gameover);
+                    scene1.addEventListener("enterframe", gameoverFunc);
+                }
             });
         };
         game.keybind('D'.charCodeAt(0), 'Dash');
         game.keybind('S'.charCodeAt(0), 'Shoot');
+        game.keybind(' '.charCodeAt(0), 'Retry');
         scene1.addEventListener("enterframe", ownMachineFunc);
         scene1.addEventListener("enterframe", bulletFunc);
         scene1.addEventListener("enterframe", enemyFunc);
+        scene1.addEventListener("enterframe", perDecision);
         game.pushScene(scene1);
     };
     game.start();
