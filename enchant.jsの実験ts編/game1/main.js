94e9ffc39f4b765125f7c11f116ded8b90e5d3c3
yousukeman3 <yousukeman3@gmail.com>
2017-09-09 21:00:10 +0900

    バグの修正と"made with enchant.js"の書き込み

--- enchant.jsの実験ts編/game1/main.js
+++ enchant.jsの実験ts編/game1/main.js
@@ -2,22 +2,23 @@
 ///<reference path="../enchant.js.d.ts"/>
 // code.9leap.net default template
 // based on enchant.js v0.7.1
-let myRandom = function (max, min) {
+let myRandom = (max, min) => {
     return Math.round(Math.random() * (max + 1 - min)) + min;
 };
 enchant();
-window.onload = function () {
+window.onload = () => {
     let game = new enchant.Core(320, 320);
     game.fps = 15;
     game.preload("bg.png", "shooting.png");
     game.onload = function () {
         let scene1 = new enchant.Scene();
         let backgroundImage = new enchant.Sprite(320, 320);
+        //backgroundImage.scale(game.width / backgroundImage.width, game.height / backgroundImage.height);
         backgroundImage.image = game.assets["bg.png"];
         scene1.addChild(backgroundImage);
         let ownMachine = new enchant.Sprite(32, 32);
         ownMachine.image = game.assets["shooting.png"];
-        ownMachine.y = 320 - 32;
+        ownMachine.y = game.height - 32;
         scene1.addChild(ownMachine);
         let score = 0;
         let scoreLabel = new enchant.Label();
@@ -26,8 +27,8 @@ window.onload = function () {
         scene1.addChild(scoreLabel);
         let gameover = new enchant.Sprite(190, 97);
         gameover.image = game.assets["end.png"];
-        gameover.x = (320 - 190) / 2;
-        gameover.y = (320 - 97) / 2;
+        gameover.x = (game.width - 190) / 2;
+        gameover.y = (game.height - 97) / 2;
         let bullets = [];
         let enemys = [];
         let waitBullet = 5;
@@ -36,7 +37,7 @@ window.onload = function () {
         let enemyCount = 0;
         let enemyrate = 50;
         let enemySpeed = 5;
-        let gameoverFunc = function () {
+        let gameoverFunc = () => {
             if (game.input.Retry) {
                 scene1.removeChild(gameover);
                 score = 0;
@@ -48,7 +49,7 @@ window.onload = function () {
                 scene1.addEventListener("enterframe", perDecision);
             }
         };
-        let ownMachineFunc = function () {
+        let ownMachineFunc = () => {
             if (game.input.right) {
                 ownMachine.x += 4;
                 if (game.input.Dash) {
@@ -68,7 +69,7 @@ window.onload = function () {
                 }
             }
         };
-        let bulletFunc = function () {
+        let bulletFunc = () => {
             //console.log(bullets.length);
             bullets.forEach(function (bullet, index, bulletAllay) {
                 if (!(bullet == null || bullet == undefined)) {
@@ -91,11 +92,11 @@ window.onload = function () {
             }
             bulletCount++;
         };
-        let enemyFunc = function () {
+        let enemyFunc = () => {
             enemys.forEach(function (enemy, index, enemyAllay) {
                 if (!(enemy == null || enemy == undefined)) {
                     enemy.y += enemySpeed;
-                    if (enemy.y > 320) {
+                    if (enemy.y > game.height) {
                         scene1.removeChild(enemy);
                         enemys.shift();
                         console.log(enemys.length);
@@ -106,12 +107,12 @@ window.onload = function () {
                 let index = enemys.push(new enchant.Sprite(32, 32)) - 1;
                 enemys[index].image = game.assets["shooting.png"];
                 enemys[index].frame = 1;
-                enemys[index].x = myRandom(0, 320 - 32);
+                enemys[index].x = myRandom(0, game.width - 32);
                 enemys[index].y = -32;
                 scene1.addChild(enemys[index]);
             }
         };
-        let perDecision = function () {
+        let perDecision = () => {
             bullets.forEach(function (bullet, indexb, bulletAllay) {
                 enemys.forEach(function (enemy, index, enemyAllay) {
                     //console.log("〇");
@@ -123,22 +124,22 @@ window.onload = function () {
                     }
                 });
             });
-            enemys.forEach(function (enemy, index, enemyAllay) {
-                if (enemy.within(ownMachine, 16) || enemy.y > 320 - 32) {
+            enemys.forEach((enemy, index, enemyAllay) => {
+                if (enemy.within(ownMachine, 16) || enemy.y > game.height - 32) {
                     console.log("gameoverだお");
                     scoreLabel.text = "score:" + score + "          スペースキーでコンテニュー";
                     scene1.removeEventListener("enterframe", ownMachineFunc);
                     scene1.removeEventListener("enterframe", bulletFunc);
                     scene1.removeEventListener("enterframe", enemyFunc);
                     scene1.removeEventListener("enterframe", perDecision);
-                    while (enemys.length > 0) {
-                        scene1.removeChild(enemys[0]);
-                        enemys.shift();
-                    }
-                    while (bullets.length > 0) {
-                        scene1.removeChild(bullets[0]);
-                        enemys.shift();
-                    }
+                    enemys.forEach((enemy, index, enemyAllay) => {
+                        scene1.removeChild(enemy);
+                    });
+                    bullets.forEach((bullet, index, bulletAllay) => {
+                        scene1.removeChild(bullet);
+                    });
+                    enemys = [];
+                    bullets = [];
                     scene1.addChild(gameover);
                     scene1.addEventListener("enterframe", gameoverFunc);
                 }
